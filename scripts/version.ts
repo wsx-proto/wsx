44f7cacebb4955283cfd3713e1133aa8a46320e3
Andrii Semenystyi <46621668+MeowningMaster@users.noreply.github.com>
2025-04-21 23:31:38 +0300

    chore: license

--- scripts/version.ts
+++ scripts/version.ts
@@ -4,10 +4,16 @@ if (!rootPackage.version) {
 	throw new Error("Root package.json does not have a version")
 }
 
+if (!rootPackage.license) {
+	throw new Error("Root package.json does not have a license")
+}
+
 const glob = new Bun.Glob("{apps,libs}/*/jsr.json")
 
 for await (const path of glob.scan()) {
 	const jsrConfig = await Bun.file(path).text()
-	const modifiedConfig = jsrConfig.replaceAll("${version}", rootPackage.version)
+	const modifiedConfig = jsrConfig
+		.replaceAll("${version}", rootPackage.version)
+		.replaceAll("${license}", rootPackage.license)
 	await Bun.write(path, modifiedConfig)
 }
